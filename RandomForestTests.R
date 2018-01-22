b8ccc4251603df474eaea614389ced7292459f05
Adam Deptula <adamdeptula@o2.pl>
2018-01-22 23:18:48 +0100

    Pokasowanie polskich znakow

--- RandomForestTests.R
+++ RandomForestTests.R
@@ -8,10 +8,10 @@ library(plotly)
 
 #packageVersion('plotly') #sprawdzenie wersji plotly
 
-#Położenie danych
+#Polozenie danych
 directory = "/home/adam/Dokumenty/17_MOW_rf/knowledge_discovery_wut"
 
-#Połączenie tabel studentów uczących się portugalskiego i matematyki:
+#Polaczenie tabel studentow uczacych sie portugalskiego i matematyki:
 d1=read.table(paste(directory,"/student-mat.csv", sep=""),sep=",",header=TRUE)
 d2=read.table(paste(directory,"/student-por.csv", sep=""),sep=",",header=TRUE)
 d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet","Dalc","Walc"))
@@ -19,14 +19,14 @@ d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","F
 grouping_table = d3[,c("Dalc", "Walc")]
 fit = kmeans(grouping_table, 2)
 d3$Alc = factor(fit$cluster)
-#Utworzenie d.alcoholics, która stanowi tabelę ostatecznie stosowaną do nauczania 
-#przez usunięcie kolumn Dalc i Walc
+#Utworzenie d.alcoholics, ktora stanowi tabele ostatecznie stosowana do nauczania 
+#przez usuniecie kolumn Dalc i Walc
 d.alc = d3[ , -which(names(d3) %in% c("Dalc","Walc"))]
 
-#Wpływ liczby drzew na wyniki modelu ------------------------------------------------------------------
-#Te badania będą dwuczęściowe:
-# 1.  Wygenerowanie lasów o różnej liczbie drzew i dla każdego lasu zbadanie błędu OOB
-# 2.  Wygenerowanie lasów o różnej liczbie drzew i wyszkolenie lasów na zbiorze treningowym
+#Wplyw liczby drzew na wyniki modelu ------------------------------------------------------------------
+#Te badania beda dwuczesciowe:
+# 1.  Wygenerowanie lasow o roznej liczbie drzew i dla kazdego lasu zbadanie bledu OOB
+# 2.  Wygenerowanie lasow o roznej liczbie drzew i wyszkolenie lasow na zbiorze treningowym
 #     a następnie testowanie na testowym.
 
 # 1.
@@ -36,30 +36,30 @@ min.trees = 2
 max.trees = 1000
 #Liczba modeli:
 count = 30
-#podstawa logarytmu wykorzystywanego do wygenerowania wektora zawierającego liczby drzew
+#podstawa logarytmu wykorzystywanego do wygenerowania wektora zawierajacego liczby drzew
 #i wykorzystywana do skalowania osi wykresu
 lbase = 2
-#Wektor przechowujący ilość drzew dla każdego z modeli:
+#Wektor przechowujacy ilosc drzew dla kazdego z modeli:
 trees = lbase^seq(log(min.trees,lbase), log(max.trees,lbase), length=count)
-#Zastosowanie logarytmów w wykładniku jest po to, aby drzewa należały do przedziału min.trees - max.trees
+#Zastosowanie logarytmow w wykladniku jest po to, aby drzewa nalezaly do przedzialu min.trees - max.trees
 
-#Zaokraglenia, aby liczby drzew były całkowite:
+#Zaokraglenia, aby liczby drzew byly calkowite:
 trees = as.integer(trees)
 
 print("Liczby drzew w testowanych modelach:")
 str(trees, vec.len=count)
 
-#Wektor, który będzie przechowywał błędy oob lasów:
+#Wektor, ktory bedzie przechowywal bledy oob lasow:
 ooberrs = vector(length=count)
 
-#Wypełnienie wektora błędów oob
+#Wypelnienie wektora bledow oob
 for(id in seq_along(trees))
 {
   #liczba drzew w lesie:
   ctree = trees[[id]]
   #Zbudowanie lasu o zadanej liczbie drzew:
-  #Ponieważ na wyniki spory wpływ ma losowość, pojedyncza wartość błędu dla danej liczby drzew
-  #będzie określana jako wartość średnia z 5 prób dla danej liczby drzew
+  #Poniewaz na wyniki spory wplyw ma losowosc, pojedyncza wartosc bledu dla danej liczby drzew
+  #będzie okreslana jako wartosc srednia z 5 prob dla danej liczby drzew
   tests = numeric(10)
   for(idt in seq_along(tests))
   {
@@ -69,27 +69,27 @@ for(id in seq_along(trees))
   #blad oob drzew liczac do itego drzewa w lesie znajduje sie na itej pozycji err.rate
   ooberrs[id] = mean(tests)
 }
-#Utworzenie tabeli zawierającej w jednej kolumnie liczbę drzew a w drugiej błąd oob odpowiadający
+#Utworzenie tabeli zawierającej w jednej kolumnie liczbe drzew a w drugiej blad oob odpowiadajacy
 #lasowi o tej liczbie drzew
 trees.vs.ooberrs = data.frame(trees,ooberrs)
 
 #write.csv(t(round(trees.vs.ooberrs,2)), file = paste(directory,"/Klasyfikacja_Wyniki/a_1.csv", sep=""))
 
-#Wykres zależności Błąd OOB(Liczba drzew w lesie)
+#Wykres zaleznosci Blad OOB(Liczba drzew w lesie)
 p.trees.oob = plot_ly(trees.vs.ooberrs, x = ~trees, y = ~ooberrs, type = 'scatter', mode = 'lines') %>%
-layout(title = "Zależnośc błędu OOB od liczby drzew w lesie", 
+layout(title = "Zaleznosc bledu OOB od liczby drzew w lesie", 
        xaxis = list(type = "log", title="Liczba drzew"),
        yaxis=list(title="Estymacja bledu OOB"))
 p.trees.oob
 
 # 2.
 
-#Podział na train i test:
+#Podzial na train i test:
 s = sample(x = nrow(d.alc), size = floor(.7*nrow(d.alc)), replace = F)
 train = d.alc[s, ]
 test  = d.alc[-s, ]
 
-#Wektory, które będą przechowywać błędy na danych testowych i OOB:
+#Wektory, ktore będą przechowywac bledy na danych testowych i OOB:
 rf.testerrs = numeric(count)
 rf.trainerrs = numeric(count)
 
@@ -115,15 +115,15 @@ for(id in 1:count)
 #rysowanie wykresu Bledu na tescie i OOB od liczby drzew w lesie
 trees.test.train = data.frame(trees,rf.testerrs,rf.trainerrs)
 p.train.test = plot_ly(data = trees.test.train, x = ~trees, type = 'scatter', mode = 'lines',
-                       y = ~rf.testerrs, name = 'Błąd na zbiorze testowym', line = list(color = 'rgb(22, 96, 167)'))%>%
-add_trace(y = ~rf.trainerrs, name = 'Błąd OOB', line = list(color = 'rgb(205, 12, 24)')) %>%
+                       y = ~rf.testerrs, name = 'Blad na zbiorze testowym', line = list(color = 'rgb(22, 96, 167)'))%>%
+add_trace(y = ~rf.trainerrs, name = 'Blad OOB', line = list(color = 'rgb(205, 12, 24)')) %>%
 layout(xaxis = list(type = "log", title="Liczba drzew"),
-       yaxis=list(title="Błąd"))
+       yaxis=list(title="Blad"))
 p.train.test
 
-#Wpływ zmiany parametru liczby atrybutów wybieranych przy podziale węzła na algorytm-------------------
-#Testowanych będzie kilka modeli lasu losowego
-print(paste("Łączna liczba parametrów: ", (ncol(d.alc)-1)))
+#Wplyw zmiany parametru liczby atrybutow wybieranych przy podziale wezla na algorytm-------------------
+#Testowanych bedzie kilka modeli lasu losowego
+print(paste("Laczna liczba parametrow: ", (ncol(d.alc)-1)))
 #Maksymalna i minimalna liczba drzew w modelach:
 min.params = 1
 max.params = (ncol(d.alc)-1)
@@ -144,7 +144,7 @@ for(id in rf.ids)
 }
 mtry.obb = data.frame(rf.ids,ooberrs)
 #write.csv(t(round(mtry.obb,3)), file = paste(directory,"/Klasyfikacja_Wyniki/b_1.csv", sep=""))
-#Wykres zależności Błąd OOB(Liczba losowo wybieranych parametrow przy podziale wezla)
+#Wykres zaleznosci Blad OOB(Liczba losowo wybieranych parametrow przy podziale wezla)
 p.mtry.obb = plot_ly(mtry.obb, x = ~rf.ids, y = ~ooberrs, type = 'scatter', mode = 'lines') %>%
   layout(title = "Błąd OOB od liczby wybieranych atrbutów", 
          xaxis = list(title="Liczba losowych atrybutów używanych do podziału węzła"),
@@ -152,12 +152,12 @@ p.mtry.obb = plot_ly(mtry.obb, x = ~rf.ids, y = ~ooberrs, type = 'scatter', mode
 p.mtry.obb
 
 
-#Wpływ długości drzew na wyniki------------------------------------------------------------------------
-#Dużo krótkich drzew
-#Mało długich drzew
-#Dużo długich drzew
+#Wplyw dlugosci drzew na wyniki------------------------------------------------------------------------
+#Duzo krotkich drzew
+#Malo dlugich drzew
+#Duzo dlugich drzew
 #nodesize
-#Będę analizował zależność BłądOOB (liczba drzew, długość drzew)
+#Bede analizowal zaleznosć BladOOB (liczba drzew, dlugosc drzew)
 
 #generacja trees - wektora liczb drzew
 min.trees=2
@@ -167,12 +167,12 @@ trees.count = 16
 trees.ids = 1:trees.count
 #podstawa logarytmu i skali wykladniczej
 lbase = 2
-#Wektor przechowujący ilość drzew dla każdej długości:
+#Wektor przechowujacy ilosc drzew dla kazdej dlugosci:
 trees = lbase^seq(log(min.trees,lbase), log(max.trees,lbase), length=trees.count)
-#Zaokraglenia, aby liczby drzew były całkowite:
+#Zaokraglenia, aby liczby drzew byly calkowite:
 trees = as.integer(trees)
 
-#generacja nodesize.counts - wektora przechowującego różne wielkosci liści
+#generacja nodesize.counts - wektora przechowujacego rozne wielkosci lisci
 min.nodesize = 1
 max.nodesize = 200
 nodesizes.count = 10
@@ -199,20 +199,20 @@ for(ns.id in seq_along(nodesizes))
     nodesizes.treescounts[ns.id,ts.id] = mean(tests.err.rate) 
     #ndbigtree to liczba wezlow drzewa dla kazdego drzewa. 
     bigtree.length = mean(tests.ndbigtree)
-    #Najwieksza srednia dlugosc drzewa - do wypisywania na konsole, tylko, żeby na szybko zarysować liczbę drzew
+    #Najwieksza srednia dlugosc drzewa - do wypisywania na konsole, tylko, zeby na szybko zarysowac liczbe drzew
     max.bigtree.length = max(c(bigtree.length,max.bigtree.length))
   }
-  print(paste("dla nodesize: ",nodesizes[ns.id],"Najwieksza średnia dł. drzew: ", max.bigtree.length))
+  print(paste("dla nodesize: ",nodesizes[ns.id],"Najwieksza srednia dl. drzew: ", max.bigtree.length))
 }
 colnames(nodesizes.treescounts) = trees
 rownames(nodesizes.treescounts) = nodesizes
 #write.csv(t(round(nodesizes.treescounts,2)), file = paste(directory,"/Klasyfikacja_Wyniki/c_1.csv", sep=""))
-#ZAMIENIC trees na trees
+
 p.nodesizes.treescounts = plot_ly(z = nodesizes.treescounts,
                                   type="heatmap", colorscale = "Greys", y=nodesizes, x=trees) %>%
-  layout(title = "Wiele krótkich drzew a niewiele długich drzew", 
+  layout(title = "Wiele krotkich drzew a niewiele dlugich drzew", 
          xaxis = list(title="Liczba drzew"),
-         yaxis=list(title="Rozmiar minimalny liści"))
+         yaxis=list(title="Rozmiar minimalny lisci"))
 p.nodesizes.treescounts
 
 
